fab78c593a0de914ac1cfc16fb3ca2696a7d75ce
Jim Coven <jattcode@gmail.com>
2015-07-19 20:26:43 +0800

    nuori

--- mod_library/src/main/java.nuori/com.mikimedia.android/nuori/Nuori.java
+++ mod_library/src/main/java.nuori/com.mikimedia.android/nuori/Nuori.java
@@ -2,13 +2,16 @@ package com.mikimedia.android.nuori;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.widget.AbsListView;
 import android.widget.ImageView;
 
 import com.mikimedia.android.R;
 
-public class Nuori implements AbsListView.OnScrollListener {
+public class Nuori {
 
     public static Nuori from(NuoriParallaxListView view) {
         return view.getNuori();
@@ -22,10 +25,13 @@ public class Nuori implements AbsListView.OnScrollListener {
 
     private boolean activated = false;
 
-    private int mDefaultImageViewHeight;
-
     Nuori(NuoriParallaxListView parent) {
         this.parent = parent;
+    }
+
+    void init(Context context, AttributeSet attrs) {
+        mDefaultImageViewHeight = context.getResources()
+                .getDimensionPixelSize(R.dimen.nuori_size_default_height);
     }
 
     public ImageView getImageView() {
@@ -68,21 +74,107 @@ public class Nuori implements AbsListView.OnScrollListener {
     private void prepare() {
         imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
 
+        parent.post(new Runnable() {
+            @Override
+            public void run() {
+                setViewsBounds(ZOOM_X2);
+            }
+        });
+
         activated = true;
     }
 
-    void init(Context context, AttributeSet attrs) {
-        mDefaultImageViewHeight = context.getResources()
-                .getDimensionPixelSize(R.dimen.nuori_size_default_height);
+
+    public final static double NO_ZOOM = 1;
+    public final static double ZOOM_X2 = 2;
+
+//    private ImageView mImageView;
+    private int mDrawableMaxHeight = -1;
+    private int mImageViewHeight = -1;
+    private int mDefaultImageViewHeight = 0;
+
+    public void setViewsBounds(double zoomRatio) {
+        if (mImageViewHeight == -1) {
+            mImageViewHeight = imageView.getHeight();
+            if (mImageViewHeight <= 0) {
+                mImageViewHeight = mDefaultImageViewHeight;
+            }
+            double ratio = ((double) imageView.getDrawable().getIntrinsicWidth()) / ((double) imageView.getWidth());
+
+            mDrawableMaxHeight = (int) ((imageView.getDrawable().getIntrinsicHeight() / ratio) * (zoomRatio > 1 ?
+                    zoomRatio : 1));
+        }
     }
 
-    @Override
-    public void onScrollStateChanged(AbsListView view, int scrollState) {
+    void onScrollChanged(int l, int t, int oldl, int oldt) {
 
+        View firstView = (View) imageView.getParent();
+        // firstView.getTop < getPaddingTop means mImageView will be covered by top padding,
+        // so we can layout it to make it shorter
+        if (firstView.getTop() < parent.getPaddingTop() && imageView.getHeight() > mImageViewHeight) {
+            imageView.getLayoutParams().height = Math.max(imageView.getHeight() - (parent.getPaddingTop() - firstView.getTop()), mImageViewHeight);
+            // to set the firstView.mTop to 0,
+            // maybe use View.setTop() is more easy, but it just support from Android 3.0 (API 11)
+            firstView.layout(firstView.getLeft(), 0, firstView.getRight(), firstView.getHeight());
+            imageView.requestLayout();
+        }
     }
 
-    @Override
-    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+    void onTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_UP) {
+            if (mImageViewHeight - 1 < imageView.getHeight()) {
+                ResetAnimimation animation = new ResetAnimimation(
+                        imageView, mImageViewHeight);
+                animation.setDuration(300);
+                imageView.startAnimation(animation);
+            }
+        }
+    }
 
+    boolean overScrollBy(int deltaX, int deltaY, int scrollX,
+                         int scrollY, int scrollRangeX, int scrollRangeY,
+                         int maxOverScrollX, int maxOverScrollY, boolean isTouchEvent) {
+
+        if (imageView.getHeight() <= mDrawableMaxHeight && isTouchEvent) {
+            if (deltaY < 0) {
+                if (imageView.getHeight() - deltaY / 2 >= mImageViewHeight) {
+                    imageView.getLayoutParams().height = imageView.getHeight() - deltaY / 2 < mDrawableMaxHeight ?
+                            imageView.getHeight() - deltaY / 2 : mDrawableMaxHeight;
+                    imageView.requestLayout();
+                }
+            } else {
+                if (imageView.getHeight() > mImageViewHeight) {
+                    imageView.getLayoutParams().height = imageView.getHeight() - deltaY > mImageViewHeight ?
+                            imageView.getHeight() - deltaY : mImageViewHeight;
+                    imageView.requestLayout();
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static class ResetAnimimation extends Animation {
+        int targetHeight;
+        int originalHeight;
+        int extraHeight;
+        View mView;
+
+        protected ResetAnimimation(View view, int targetHeight) {
+            this.mView = view;
+            this.targetHeight = targetHeight;
+            originalHeight = view.getHeight();
+            extraHeight = this.targetHeight - originalHeight;
+        }
+
+        @Override
+        protected void applyTransformation(float interpolatedTime,
+                                           Transformation t) {
+
+            int newHeight;
+            newHeight = (int) (targetHeight - extraHeight * (1 - interpolatedTime));
+            mView.getLayoutParams().height = newHeight;
+            mView.requestLayout();
+        }
     }
 }
--- mod_library/src/main/java.nuori/com.mikimedia.android/nuori/NuoriParallaxListView.java
+++ mod_library/src/main/java.nuori/com.mikimedia.android/nuori/NuoriParallaxListView.java
@@ -5,21 +5,9 @@ import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.Transformation;
-import android.widget.ImageView;
 import android.widget.ListView;
 
 public class NuoriParallaxListView extends ListView {
-
-    public final static double NO_ZOOM = 1;
-    public final static double ZOOM_X2 = 2;
-
-    private ImageView mImageView;
-    private int mDrawableMaxHeight = -1;
-    private int mImageViewHeight = -1;
-    private int mDefaultImageViewHeight = 0;
 
     private final Nuori nuori = new Nuori(this);
 
@@ -28,15 +16,7 @@ public class NuoriParallaxListView extends ListView {
     }
 
     void setNuori(Nuori nuori) {
-        mImageView = nuori.getImageView();
         addHeaderView(nuori.getHeaderView());
-
-        post(new Runnable() {
-            @Override
-            public void run() {
-                setViewsBounds(ZOOM_X2);
-            }
-        });
     }
 
     public NuoriParallaxListView(Context context) {
@@ -60,23 +40,13 @@ public class NuoriParallaxListView extends ListView {
         nuori.init(context, attrs);
     }
 
-    private interface OnOverScrollByListener {
-        boolean overScrollBy(int deltaX, int deltaY, int scrollX,
-                             int scrollY, int scrollRangeX, int scrollRangeY,
-                             int maxOverScrollX, int maxOverScrollY, boolean isTouchEvent);
-    }
-
-    private interface OnTouchEventListener {
-        void onTouchEvent(MotionEvent ev);
-    }
-
     @Override
     protected boolean overScrollBy(int deltaX, int deltaY, int scrollX,
                                    int scrollY, int scrollRangeX, int scrollRangeY,
                                    int maxOverScrollX, int maxOverScrollY, boolean isTouchEvent) {
         boolean isCollapseAnimation = false;
 
-        isCollapseAnimation = scrollByListener.overScrollBy(deltaX, deltaY,
+        isCollapseAnimation = nuori.overScrollBy(deltaX, deltaY,
                 scrollX, scrollY, scrollRangeX, scrollRangeY, maxOverScrollX,
                 maxOverScrollY, isTouchEvent)
                 || isCollapseAnimation;
@@ -89,97 +59,13 @@ public class NuoriParallaxListView extends ListView {
     @Override
     protected void onScrollChanged(int l, int t, int oldl, int oldt) {
         super.onScrollChanged(l, t, oldl, oldt);
-        View firstView = (View) mImageView.getParent();
-        // firstView.getTop < getPaddingTop means mImageView will be covered by top padding,
-        // so we can layout it to make it shorter
-        if (firstView.getTop() < getPaddingTop() && mImageView.getHeight() > mImageViewHeight) {
-            mImageView.getLayoutParams().height = Math.max(mImageView.getHeight() - (getPaddingTop() - firstView.getTop()), mImageViewHeight);
-            // to set the firstView.mTop to 0,
-            // maybe use View.setTop() is more easy, but it just support from Android 3.0 (API 11)
-            firstView.layout(firstView.getLeft(), 0, firstView.getRight(), firstView.getHeight());
-            mImageView.requestLayout();
-        }
+        nuori.onScrollChanged(l, t, oldl, oldt);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        touchListener.onTouchEvent(ev);
+        nuori.onTouchEvent(ev);
         return super.onTouchEvent(ev);
     }
 
-    public void setViewsBounds(double zoomRatio) {
-        if (mImageViewHeight == -1) {
-            mImageViewHeight = mImageView.getHeight();
-            if (mImageViewHeight <= 0) {
-                mImageViewHeight = mDefaultImageViewHeight;
-            }
-            double ratio = ((double) mImageView.getDrawable().getIntrinsicWidth()) / ((double) mImageView.getWidth());
-
-            mDrawableMaxHeight = (int) ((mImageView.getDrawable().getIntrinsicHeight() / ratio) * (zoomRatio > 1 ?
-                    zoomRatio : 1));
-        }
-    }
-
-    private OnOverScrollByListener scrollByListener = new OnOverScrollByListener() {
-        @Override
-        public boolean overScrollBy(int deltaX, int deltaY, int scrollX,
-                                    int scrollY, int scrollRangeX, int scrollRangeY,
-                                    int maxOverScrollX, int maxOverScrollY, boolean isTouchEvent) {
-            if (mImageView.getHeight() <= mDrawableMaxHeight && isTouchEvent) {
-                if (deltaY < 0) {
-                    if (mImageView.getHeight() - deltaY / 2 >= mImageViewHeight) {
-                        mImageView.getLayoutParams().height = mImageView.getHeight() - deltaY / 2 < mDrawableMaxHeight ?
-                                mImageView.getHeight() - deltaY / 2 : mDrawableMaxHeight;
-                        mImageView.requestLayout();
-                    }
-                } else {
-                    if (mImageView.getHeight() > mImageViewHeight) {
-                        mImageView.getLayoutParams().height = mImageView.getHeight() - deltaY > mImageViewHeight ?
-                                mImageView.getHeight() - deltaY : mImageViewHeight;
-                        mImageView.requestLayout();
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-    };
-
-    private OnTouchEventListener touchListener = new OnTouchEventListener() {
-        @Override
-        public void onTouchEvent(MotionEvent ev) {
-            if (ev.getAction() == MotionEvent.ACTION_UP) {
-                if (mImageViewHeight - 1 < mImageView.getHeight()) {
-                    ResetAnimimation animation = new ResetAnimimation(
-                            mImageView, mImageViewHeight);
-                    animation.setDuration(300);
-                    mImageView.startAnimation(animation);
-                }
-            }
-        }
-    };
-
-    public class ResetAnimimation extends Animation {
-        int targetHeight;
-        int originalHeight;
-        int extraHeight;
-        View mView;
-
-        protected ResetAnimimation(View view, int targetHeight) {
-            this.mView = view;
-            this.targetHeight = targetHeight;
-            originalHeight = view.getHeight();
-            extraHeight = this.targetHeight - originalHeight;
-        }
-
-        @Override
-        protected void applyTransformation(float interpolatedTime,
-                                           Transformation t) {
-
-            int newHeight;
-            newHeight = (int) (targetHeight - extraHeight * (1 - interpolatedTime));
-            mView.getLayoutParams().height = newHeight;
-            mView.requestLayout();
-        }
-    }
 }
