cf7241c446704b46847b71427ee2c4778fd419da
Jim Coven <jattcode@gmail.com>
2015-07-20 20:51:19 +0800

    wip

--- mod_sample/src/main/java.nuori/com.mikimedia.demo/nuori/NuoriScrollViewActivity.java
+++ mod_sample/src/main/java.nuori/com.mikimedia.demo/nuori/NuoriScrollViewActivity.java
@@ -27,25 +27,25 @@ public class NuoriScrollViewActivity extends AppCompatActivity {
         NuoriParallaxScrollView scrollView = (NuoriParallaxScrollView) findViewById(R.id.scroll_view);
         final Nuori nuori = Nuori.from(scrollView)
                 .setImageView(imageView)
-                .into();
+                .into(false);
 
         final Target target = new Target() {
             @Override
             public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
                 imageView.setImageBitmap(bitmap);
-//                nuori.notifyViewBoundsChanged();
+                nuori.notifyViewBoundsChanged();
             }
 
             @Override
             public void onBitmapFailed(Drawable errorDrawable) {
                 imageView.setImageDrawable(errorDrawable);
-//                nuori.notifyViewBoundsChanged();
+                nuori.notifyViewBoundsChanged();
             }
 
             @Override
             public void onPrepareLoad(Drawable placeHolderDrawable) {
                 imageView.setImageDrawable(placeHolderDrawable);
-//                nuori.notifyViewBoundsChanged();
+                nuori.notifyViewBoundsChanged();
             }
         };
 
@@ -53,9 +53,9 @@ public class NuoriScrollViewActivity extends AppCompatActivity {
 //        imageView.setImageResource(R.mipmap.ic_launcher);
 //        imageView.setImageResource(R.mipmap.horizontal_image);
 //        imageView.setImageResource(R.mipmap.vertical_long);
-//        ImageLoader.with(this).loadSampleImage(target);
+        ImageLoader.with(this).loadSampleImage(target);
 //        ImageLoader.with(this).loadSampleImage(target, 4);
-        ImageLoader.with(this).loadSampleImage(imageView);
+//        ImageLoader.with(this).loadSampleImage(imageView);
 //        ImageLoader.with(this).loadSampleImage(imageView, 4);
 
 
